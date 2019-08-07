37ad0cd4faaee9736bf7ff334a3fadcc8c2bc824
Elizaveta Zakharova <lizazakh9@gmail.com>
2019-08-07 18:22:58 +0300

    WIP

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const server = require('browser-sync').create();
+
+gulp.task('style', function () {
+    gulp.src
+    
+});
+
