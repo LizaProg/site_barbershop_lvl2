3dce85778df6f5b31203ca46f042aff5d9a925ff
Elizaveta Zakharova <lizazakh9@gmail.com>
2019-08-07 23:50:19 +0300

    pakage

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,34 @@
 const gulp = require('gulp');
-const sass = require('sass');
+const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const server = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('style', function () {
-    gulp.src
-    
+    return gulp.src('sass/*.scss')
+        .pipe(plumber())
+        .pipe(concat('app.scss'))
+        .pipe(sass.sync({outputStyle: 'compressed'}))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream())
+
 });
 
+gulp.task('server', gulp.series('style', function(){
+    browserSync.init({
+        server: '.',
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+
+    gulp.watch('sass/**/*.{scss, sass}', gulp.series('style'));
+    gulp.watch('*.html').on('change', browserSync.reload);
+}));
